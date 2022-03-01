9c86e718b468c0c9e6eeb3fc3eaa39d4fe856e7b
ciyuanhuixing <ciyuanhuixing@qq.com>
2022-03-01 16:03:14 +0800

    2022-03-01 16:03:14

--- 未分类/新建文章并打开.bat
+++ 未分类/新建文章并打开.bat
@@ -1,6 +1,6 @@
 set hh=%time:~0,2%
 if /i %hh% LSS 10 (set hh=0%time:~1,1%)
-set dt=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%-%hh%%TIME:~3,2%
+set dt=%DATE:~0,4%%DATE:~5,2%-%DATE:~8,2%-%hh%%TIME:~3,2%
 set file=%dt%-.md
 
 if not exist %file% (
